bbdbfac9f64f4e510c085b04ea3f890ed87b6d0f
Gustavo Rodrigues <gustavo.fabiano.2010@hotmail.com>
2022-08-20 21:40:46 -0300

    Organize project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import catalog from './assets/catalog/catalog.json';
-
 function App() {
   return (
     <div className="App">
-      { 
-        catalog.map(({ name, image, id }) => (
-          <li key={id}>
-            {name}
-            <img src={require(`./assets/images/${image}`)} alt={image} />
-          </li>
-        )) 
-      }
+      
     </div>
   );
 }
