d331048e0bac4bce73e59f05469076b02e63d83a
Gustavo Rodrigues <gustavo.fabiano.2010@hotmail.com>
2022-08-25 14:31:07 -0300

    Add discount in the products

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -6,29 +6,48 @@ function ListProducts({ products }) {
   return (
     <ul className="list-products">
       {
-        products.map(({ id, name, image, units, stars, price }) => (
-          <li key={id} className="product">
-            <div className="container-image">
-              <img src={require(`../../assets/images/${image}`)} alt={`Produto: ${name}`} />
-            </div>
-            <div className="container-name">
-              <span>{name}</span>
-            </div>
-            <div className="container-units">
-              <small>{units} unidades restantes</small>
-            </div>
-            <div className="container-rating">
-              {
-                <StarRatingList stars={stars} />
-              }
-            </div>
-            <div className="container-price">
-              <span>
-                { price.toLocaleString('pt-br', { style: "currency", currency: "BRL" }) }
-              </span>
-            </div>
-          </li>
-        ))
+        products.map(({ id, name, image, units, stars, price, discount }) => {
+          const currencyFormat = { style: "currency", currency: "BRL" };
+
+          const priceWithDiscount = price - (price * (discount / 100));
+
+          return (
+            <li key={id} className="product">
+              <div className="container-image">
+                <img src={require(`../../assets/images/${image}`)} alt={`Produto: ${name}`} />
+              </div>
+              <div className="container-name">
+                <span>{name}</span>
+              </div>
+              <div className="container-units">
+                <small>{units} unidades restantes</small>
+              </div>
+              <div className="container-rating">
+                {
+                  <StarRatingList stars={stars} />
+                }
+              </div>
+              <div className="container-price">
+                {
+                  discount && (
+                    <div className="price-with-discount">
+                      De <del>{ price.toLocaleString('pt-br', currencyFormat) }</del> <span>(↓{ discount }%)</span> por:
+                    </div>
+                  )
+                }
+                <div>
+                  <span>
+                    { 
+                      discount 
+                        ? priceWithDiscount.toLocaleString('pt-br', currencyFormat) 
+                        : price.toLocaleString('pt-br', currencyFormat) 
+                    }
+                  </span>
+                </div>
+              </div>
+            </li>
+          )
+        })
       }
     </ul>
   )
