08551c6dea9c5a5650b2c889e6af4614ece6e3c6
Gustavo Rodrigues <gustavo.fabiano.2010@hotmail.com>
2022-08-25 11:21:45 -0300

    Change star rating order

--- src/hooks/useFilter.js
+++ src/hooks/useFilter.js
@@ -45,12 +45,11 @@ function useFilter(products) {
   ]
 
   const filledOrUnfilledStars = [
-    [false, false, false ,false, false],
-    [true, false, false ,false, false],
-    [true, true, false ,false, false],
-    [true, true, true ,false, false],
+    [true, true, true ,true, true],
     [true, true, true ,true, false],
-    [true, true, true ,true, true]
+    [true, true, true ,false, false],
+    [true, true, false ,false, false],
+    [true, false, false ,false, false]
   ]
 
   const stars = filledOrUnfilledStars.map(item => (
