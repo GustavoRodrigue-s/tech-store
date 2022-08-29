8abf6ff2068049f08d76e6e36b9c9278d91d40b5
Gustavo Rodrigues <gustavo.fabiano.2010@hotmail.com>
2022-08-29 17:26:26 -0300

    Split home

---
+++ src/pages/Home/Main.jsx
@@ -0,0 +1,77 @@
+import { useState } from 'react';
+
+import FilterBar from '../../components/FilterBar';
+import SectionProducts from '../../components/SectionProducts';
+
+import { FilterProvider } from '../../store/FilterContext';
+
+function Main({ products, filteredProducts }) {
+  const [filters, setFilters] = useState({
+    departments: [],
+    price: [],
+    discount: [],
+    rating: []
+  });
+
+  const handleFilterChange = (isChecked, { filterCategory, label, products }) => {
+    isChecked
+      ? setFilters({ ...filters, [filterCategory]: [...filters[filterCategory], { label, products }] })
+      : setFilters(() => {
+        const newFilter = filters[filterCategory].filter(filter => filter.label !== label);
+
+        return { ...filters, [filterCategory]: newFilter };
+      });
+  }
+
+  const getProductsToDisplay = () => {
+    const { departments, rating, discount, price } = filters;
+
+    const allFilters = [departments, rating, discount, price];
+
+    const mainFilter = allFilters.find(filter => filter.length ? filter : null);
+    
+    if (!mainFilter) {
+      return products
+    }
+
+    const currentProducts = mainFilter.reduce((acc, { products }) => acc = [...acc, ...products], []);
+
+    const commonProducts = [rating, discount, price].map(filter => 
+      filter !== mainFilter 
+        ? filter.reduce((acc, { products }) => acc = [...acc, ...products], []) 
+        : []
+    );
+
+    const hasOthersFilters = commonProducts.find(filter => filter.length);
+    
+    if (!hasOthersFilters) {
+      return currentProducts;
+    }
+
+    const filteredProducts = currentProducts.filter(({ id }) => 
+      commonProducts.every(products => products.length 
+        ? products.find(product => product.id === id) 
+        : true
+      )
+    );
+
+    return filteredProducts
+  }
+
+  const productsToDisplay = getProductsToDisplay();
+
+  return (
+    <main>
+      <FilterProvider value={{ filteredProducts, handleFilterChange }}>
+        <aside>
+          <FilterBar />
+        </aside>
+        <section>
+          <SectionProducts products={productsToDisplay} />
+        </section>
+      </FilterProvider>
+    </main>
+  )
+}
+
+export default Main
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,88 +1,22 @@
-import { useState } from 'react';
-
 import useCatalog from '../../hooks/useCatalog';
 import useFilter from '../../hooks/useFilter';
 
 import Header from '../../components/Header';
-import FilterBar from '../../components/FilterBar';
-import SectionProducts from '../../components/SectionProducts';
-
-import { FilterProvider } from '../../store/FilterContext';
+import Main from './Main';
 
 import "./style.css";
 
-export default function Home() {
+function Home() {
   const products = useCatalog();
-
-  const [filters, setFilters] = useState({
-    departments: [],
-    price: [],
-    discount: [],
-    rating: []
-  });
-
-  const handleFilterChange = (isChecked, { filterCategory, label, products }) => {
-    isChecked
-      ? setFilters({ ...filters, [filterCategory]: [...filters[filterCategory], { label, products }] })
-      : setFilters(() => {
-        const newFilter = filters[filterCategory].filter(filter => filter.label !== label);
-
-        return { ...filters, [filterCategory]: newFilter };
-      });
-  }
-
-  const returnCurrentProductsToDisplay = () => {
-    const { departments, rating, discount, price } = filters;
-
-    const allFilters = [departments, rating, discount, price];
-
-    const mainFilter = allFilters.find(filter => filter.length ? filter : null);
-    
-    if (!mainFilter) {
-      return products
-    }
-
-    const currentProducts = mainFilter.reduce((acc, { products }) => acc = [...acc, ...products], []);
-
-    const commonProducts = [rating, discount, price].map(filter => 
-      filter !== mainFilter 
-        ? filter.reduce((acc, { products }) => acc = [...acc, ...products], []) 
-        : []
-    );
-
-    const hasOthersFilters = commonProducts.find(filter => filter.length);
-    
-    if (!hasOthersFilters) {
-      return currentProducts;
-    }
-
-    const filteredProducts = currentProducts.filter(({ id }) => 
-      commonProducts.every(products => products.length 
-        ? products.find(product => product.id === id) 
-        : true
-      )
-    );
-
-    return filteredProducts
-  }
-
-  const productsToDisplay = returnCurrentProductsToDisplay();
 
   const filteredProducts = useFilter(products);
 
   return (
     <>
       <Header />
-      <main>
-        <FilterProvider value={{ filteredProducts, handleFilterChange }}>
-          <aside>
-            <FilterBar />
-          </aside>
-          <section>
-            <SectionProducts products={productsToDisplay} />
-          </section>
-        </FilterProvider>
-      </main>
+      <Main products={products} filteredProducts={filteredProducts} />
     </>
   )
 }
+
+export default Home
